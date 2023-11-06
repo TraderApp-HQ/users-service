3b05be9ab435e858bf98b3183465cf6d5ad4ce94
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-11-06 08:55:35 +0100

    changed back absolute path

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,6 +1,6 @@
 import swaggerJsdoc from 'swagger-jsdoc';
 import { version } from "../../package.json"
-import { createAuthLogin, createAuthLoginBody, createAuthLogout, createAuthLogoutBody, createAuthPasswordLinkReset, createAuthPasswordLinkResetBody, createAuthPasswordReset, createAuthPasswordResetBody, createAuthRefreshToken, createAuthRefreshTokenBody, createAuthSignup, createAuthSignupBody,  } from '@/swagger';
+import { createAuthLogin, createAuthLoginBody, createAuthLogout, createAuthLogoutBody, createAuthPasswordLinkReset, createAuthPasswordLinkResetBody, createAuthPasswordReset, createAuthPasswordResetBody, createAuthRefreshToken, createAuthRefreshTokenBody, createAuthSignup, createAuthSignupBody,  } from '../swagger';
 
 const options: swaggerJsdoc.Options = {
   swaggerDefinition: {
