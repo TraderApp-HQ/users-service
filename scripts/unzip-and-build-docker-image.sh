86416ab9c1f0df0ea64457f6feb68f1029dc19ed
SpiffGreen <spiffjekeygreen@gmail.com>
2024-05-16 07:55:27 +0100

    fix: path issue

--- scripts/unzip-and-build-docker-image.sh
+++ scripts/unzip-and-build-docker-image.sh
@@ -5,6 +5,8 @@ IMAGE_NAME="user-service"
 CONTAINER_NAME="user-service"
 DOCKERFILE_PATH="."
 
+cd ~/app
+
 # Stop and remove the container if it exists
 if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
     echo "Stopping existing container: $CONTAINER_NAME"
