396e771b90187313ddc81fe7f578479098b34f32
Dike Ajah <ekidhaja@gmail.com>
2024-05-19 22:13:53 +0100

    minor change to script

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ function startServer() {
 
 	// health check
 	app.get("/ping", async (req, res, next) => {
-		res.status(200).json(apiResponseHandler({ message: "pong pong" }));
+		res.status(200).json(apiResponseHandler({ message: "pong" }));
 	});
 
 	// handle errors
