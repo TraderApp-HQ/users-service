9dd4a35977c13d1a8b68c87393a5067c6387663a
Dike Ajah <ekidhaja@gmail.com>
2024-05-19 23:26:08 +0100

    new deployment

--- src/app.ts
+++ src/app.ts
@@ -72,7 +72,7 @@ function startServer() {
 
 	// health check
 	app.get("/ping", async (req, res, next) => {
-		res.status(200).json(apiResponseHandler({ message: "pong" }));
+		res.status(200).json(apiResponseHandler({ message: "pong again" }));
 	});
 
 	// handle errors
